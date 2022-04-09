ef645e49e86cb6f6f19f2263a65e73627e63464c
Lao <Lao@gmail.com>
2022-04-09 15:40:27 +0100

    Deuxièm projet SalariApp: Template

---
+++ Entreprise/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Login, name='Login'),
+    path('Dash', views.Dash, name='Dash'),
+
+]
---
+++ ProjetSalariApp/info.py
@@ -0,0 +1,6 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'salariapp21@gmail.com'
+EMAIL_HOST_PASSWORD = 'salariapp2021999???'
+EMAIL_PORT = 587
+
